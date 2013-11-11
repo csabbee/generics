cd33ea67fa77d5345a7734d462bba1d13be0714e
Csaba Valyi <csaba_valyi@epam.com>
2013-11-11 13:54:27 +0100

    enum reloaded

--- src/main/typesystem/wildcardsbounds/enums/Department.java
+++ src/main/typesystem/wildcardsbounds/enums/Department.java
@@ -2,5 +2,33 @@ package typesystem.wildcardsbounds.enums;
 
 // decompile with JAD
 public enum Department {
-	JAVA, DOTNET, ENGINEERING_SERVICES;
+	JAVA(true){
+
+        @Override
+        public void printLanguages() {
+           System.out.println("Java, Groovy, Clojure, Scala");
+        }
+	    
+	}, DOTNET(false){
+
+        @Override
+        public void printLanguages() {
+            System.out.println("C#, F#");
+        }
+	    
+	}, ENGINEERING_SERVICES(true){
+
+        @Override
+        public void printLanguages() {
+            System.out.println("ant, maven");
+        }
+	    
+	};
+	private boolean great;
+	
+	private Department(boolean great){
+	    this.great = great;
+	}
+	
+	public abstract void printLanguages();
 }
